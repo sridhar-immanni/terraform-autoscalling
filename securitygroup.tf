70884ffcfea0f15ff2bc015febbcd6f6a8ecac8f
sridhar-immanni <sridharimmanni86@gmail.com>
2021-09-11 11:25:51 +0530

    first

---
+++ securitygroup.tf
@@ -0,0 +1,73 @@
+resource "aws_security_group" "myinstance" {
+  vpc_id      = aws_vpc.main.id
+  name        = "myinstance"
+  description = "security group for my instance"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.elb-securitygroup.id]
+  }
+
+  tags = {
+    Name = "myinstance"
+  }
+}
+
+resource "aws_security_group" "elb-securitygroup" {
+  vpc_id      = aws_vpc.main.id
+  name        = "elb"
+  description = "security group for load balancer"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "elb"
+  }
+}
+
+resource "aws_security_group" "allow-mysql" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-mysql"
+  description = "allow-mariadb"
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.example-instance.id] # allowing access from our example instance
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
+  }
+  tags = {
+    Name = "allow-mysql"
+  }
+}
